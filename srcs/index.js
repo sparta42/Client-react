24e0d5e4cbabbe013e779e43f9535bb99e2f0e72
Ubuntu <ubuntu@ip-172-31-12-65.ap-northeast-2.compute.internal>
2021-06-09 04:02:00 +0000

    feat : static_map connect

--- srcs/index.js
+++ srcs/index.js
@@ -1,24 +1,19 @@
-const app = elem => {
-  const state = {text: 1};
+import '../public/style.scss'
+const mapsUrl = 'https://naveropenapi.apigw.ntruss.com/map-static/v2/raster-cors?'
+let width = 400
+let height = 350
+let centerX = 127.0646955
+let centerY = 37.4882247
+let level = 16
+let apiKeyID = '9cdskm4clv'
 
-  const $elem = document.querySelector(elem);
 
-  const render = () => {
-    $elem.innerHTML = `
-      <h1>
-        ${state.text}
-      </h1>
-    `;
-  };
+const elem = document.createElement('div')
 
-  $elem.addEventListener('click', () => {
-    state.text = state.text + 1;
-    render();
-  });
+elem.innerHTML = `
+<div id='maps_api'>
+    <img src=${mapsUrl}w=${width}&h=${height}&center=${centerX},${centerY}&scale=2&level=${level}&X-NCP-APIGW-API-KEY-ID=${apiKeyID}>
+</div>
+`
 
-  render();
-
-  return $elem;
-};
-
-app('body');
+document.querySelector('div').appendChild(elem)
