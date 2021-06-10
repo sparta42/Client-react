2d7f1a5bfb59061d4bd7b374d2ca02ea93cd1176
kkang <kkang@student.42seoul.kr>
2021-06-11 00:32:42 +0900

    feat: Connect naver map api & Display static map
    - Display naver static map using naver static map API

--- srcs/index.js
+++ srcs/index.js
@@ -1,24 +1,55 @@
-const app = elem => {
-  const state = {text: 1};
+import { $ } from "./helpers.js";
+import { API_KEY_ID } from "./key.js";
+import { makeMapTemplate } from "./template.js";
 
-  const $elem = document.querySelector(elem);
-
-  const render = () => {
-    $elem.innerHTML = `
-      <h1>
-        ${state.text}
-      </h1>
-    `;
+const app = (elem) => {
+  const state = {
+    width: 800,
+    height: 800,
+    level: 15,
+    currentLatitude: 37.4882247,
+    currentLongitude: 127.0652055,
+    isInit: true,
   };
 
-  $elem.addEventListener('click', () => {
-    state.text = state.text + 1;
+  const showPositionOntheMap = (crd, state) => {
+    const { latitude, longitude } = crd;
+    const mapURI = `https://naveropenapi.apigw.ntruss.com/map-static/v2/raster-cors?w=${state.width}&h=${state.height}&center=${longitude},${latitude}&level=${state.level}&X-NCP-APIGW-API-KEY-ID=${API_KEY_ID}`;
+
+    $("#map-container").innerHTML = makeMapTemplate(mapURI, state);
+    if (navigator.geolocation && !state.isInit) {
+      $("#marker").style.display = "block";
+    }
+  };
+
+  const render = () => {
+    showPositionOntheMap(
+      {
+        longitude: state.currentLongitude,
+        latitude: state.currentLatitude,
+      },
+      state
+    );
+  };
+
+  const $elem = $(elem);
+
+  const init = () => {
+    const $app = document.createElement("div");
+    const $mapContainer = document.createElement("div");
+
+    $app.setAttribute("id", "app");
+    $mapContainer.setAttribute("id", "map-container");
+
+    $app.appendChild($mapContainer);
+    $elem.appendChild($app);
+
     render();
-  });
+    state.isInit = false;
+  };
 
-  render();
-
+  init();
   return $elem;
 };
 
-app('body');
+app("body");
