8cae37525afc263c9e16ced54844c39449b8988e
kkang <kkang@student.42seoul.kr>
2021-06-11 00:45:28 +0900

    feat: Implement getcurrentposition feature
    - geolocationAPI 를 활용한 간단한 현위치 조회 기능

--- srcs/index.js
+++ srcs/index.js
@@ -22,6 +22,47 @@ const app = (elem) => {
     }
   };
 
+  const getPositionInfos = (position) => {
+    const crd = position.coords;
+
+    state.currentLatitude = crd.latitude;
+    state.currentLongitude = crd.longitude;
+    showPositionOntheMap(crd, state);
+  };
+
+  const requestLocationPermission = () => {
+    console.log("test");
+    if (navigator.geolocation) {
+      const options = {
+        enableHighAccuracy: true,
+        timeout: 3000,
+        maximumAge: 0,
+      };
+      navigator.geolocation.getCurrentPosition(
+        getPositionInfos,
+        (err) => {
+          console.error(err);
+          showPositionOntheMap({
+            latitude: state.currentLatitude,
+            longitude: state.currentLongitude,
+          });
+        },
+        options
+      );
+    } else {
+      alert("지도 서비스를 이용할 수 없는 브라우지입니다.");
+    }
+  };
+
+  const handleWheel = (event) => {
+    if (event.target.id !== "mapImage") return;
+    if (event.deltaY < 0) {
+      zoomInMap(state);
+      return;
+    }
+    zoomOutMap(state);
+  };
+
   const render = () => {
     showPositionOntheMap(
       {
@@ -37,12 +78,19 @@ const app = (elem) => {
   const init = () => {
     const $app = document.createElement("div");
     const $mapContainer = document.createElement("div");
+    const $getLocationButton = document.createElement("button");
 
     $app.setAttribute("id", "app");
     $mapContainer.setAttribute("id", "map-container");
+    $getLocationButton.setAttribute("id", "locationBtn");
+    $getLocationButton.innerText = "현재 위치는?";
 
     $app.appendChild($mapContainer);
+    $app.appendChild($getLocationButton);
     $elem.appendChild($app);
+
+    $getLocationButton.addEventListener("click", requestLocationPermission);
+    $mapContainer.addEventListener("wheel", handleWheel);
 
     render();
     state.isInit = false;
