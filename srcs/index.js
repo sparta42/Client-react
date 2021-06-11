9b6ac3a5722357da5df0eaa5eb21388d52a49213
seonja <seonja@seonjaui-MacBookPro.local>
2021-06-11 10:22:21 +0900

    map zoom in and out with wheel and click function

--- srcs/index.js
+++ srcs/index.js
@@ -1,20 +1,36 @@
 const app = elem => {
-  const state = {text: 1};
+  const center = "127.1054221,37.3591614";
+  const clientId = "1zszamdg6u"
+  const state = {
+    level : 1
+  }
 
   const $elem = document.querySelector(elem);
-
   const render = () => {
-    $elem.innerHTML = `
-      <h1>
-        ${state.text}
-      </h1>
-    `;
+    //$elem.appendChild(staticMap());
   };
 
-  $elem.addEventListener('click', () => {
-    state.text = state.text + 1;
-    render();
-  });
+  let map = document.querySelector('#theMap');
+  
+  const clickFunc = () => {
+    if (state.level < 16) {
+      state.level = state.level + 1;
+      map.setAttribute('src', `https://naveropenapi.apigw.ntruss.com/map-static/v2/raster-cors?w=300&h=300&center=${center}&level=${Math.round(state.level)}&X-NCP-APIGW-API-KEY-ID=${clientId}`);
+    }
+  }
+
+  const zoom = (e) => {
+    e.preventDefault();
+
+    if (state.level < 16.6 && e.deltaY < -1)
+      state.level = state.level + 0.1;
+    else if (state.level > 0.4 && e.deltaY > 1)
+      state.level = state.level - 0.1;
+    map.setAttribute('src', `https://naveropenapi.apigw.ntruss.com/map-static/v2/raster-cors?w=300&h=300&center=${center}&level=${Math.round(state.level)}&X-NCP-APIGW-API-KEY-ID=${clientId}`);
+  }
+
+  document.getElementById('theMap').addEventListener("click", clickFunc);
+  document.getElementById('theMap').addEventListener("wheel", zoom);
 
   render();
 
---
+++ srcs/router.js
@@ -0,0 +1,50 @@
+//참고자료 : https://www.youtube.com/watch?v=6BozpmSjk-Y&ab_channel=dcodedcode
+import Dashboard from "./view/Dashboard.js";
+
+const navigateTo = url => {
+	history.pushState(null, null, url);
+	router();
+}
+
+const router = async () => {
+	const routes = [
+		{path: "/", view: Dashboard},
+		{path: "/posts", view: () => console.log("Viewing Posts")},
+		{path: "/settings", view: () => console.log("Viewing Settings")}
+	];
+
+	const potentialMatches = routes.map(route => {
+		return {
+			route: route,
+			isMatch: location.pathname === route.path
+		}
+	})
+
+	let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch);
+
+	if (!match) {
+		match = {
+			route: routes[0],
+			isMatch: true
+		}
+	}
+
+	const view = new match.route.view();
+
+	document.querySelector("#app").innerHTML = await view.getHtml();
+
+	console.log(match.route.view());
+}
+
+window.addEventListener("popstate", router);
+
+document.addEventListener("DOMContentLoaded", () => {
+	document.body.addEventListener("click", e => {
+		if (e.target.matches("[data-link]")) {
+			e.preventDefault();
+			navigateTo(e.target.href);
+		}
+	})
+
+	router();
+})
