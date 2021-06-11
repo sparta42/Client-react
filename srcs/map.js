0b27fa3f15985a4b3aa90a83fbc05d61d722e4fb
seo2im <seo2im6492@gmail.com>
2021-06-11 09:33:48 +0900

    feat: delete before image

--- srcs/map.js
+++ srcs/map.js
@@ -2,15 +2,16 @@ import * as env from '../.env';
 import draggable from './lib/dragable';
 import calculateP2L from './lib/calculator';
 
-const map = (width, height, lat, lot, zoom) => {
-  let state = {
+const map = (width, height, latitude, longtitude, zoom) => {
+  const state = {
     width,
     height,
-    lat,
-    lot,
+    lat: latitude,
+    lot: longtitude,
     zoom
   };
 
+  const wrapper = document.querySelector('.map');
   const $elem = document.createElement('img');
 
   const render = () => {
@@ -19,25 +20,19 @@ const map = (width, height, lat, lot, zoom) => {
                 + `&X-NCP-APIGW-API-KEY-ID=${env.CLIENT_ID}`;
   };
 
-  const setState = (key, value) => {
-    state = Object.assign({}, state, {[key]: value});
-  };
-
   const changeCenter = () => {
     const left = parseInt($elem.style.getPropertyValue('--left'), 10);
     const top = parseInt($elem.style.getPropertyValue('--top'), 10);
 
-    console.log((state.width / 2) - (isNaN(left) ? 0 : left + 250));
-    console.log((state.height / 2) - (isNaN(top) ? 0 : top + 250));
-
-    const { lat, lot } = calculateP2L({
+    const {lat, lot} = calculateP2L({
       lat: state.lat,
       lot: state.lot
     }, state.zoom, state.width, state.height, {
       x: (state.width / 2) - (isNaN(left) ? 0 : (left + 250) / 2),
       y: (state.height / 2) - (isNaN(top) ? 0 : (top + 250) / 2)
     });
-    document.querySelector('.map').appendChild(map(1000, 1000, lat, lot, 16));
+    wrapper.appendChild(map(1000, 1000, lat, lot, 16));
+    setTimeout(() => wrapper.removeChild(wrapper.firstChild), 500);
   };
 
   const eventBind = () => {
